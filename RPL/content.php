ff79f995062f82e3e054feef44b36a894a6b1708
dwi-novitasari <dwinovitasari@s1.informatika.unsyiah.ac.id>
2013-06-09 12:11:42 +0700

    diupload oleh dwi

--- RPL/content.php
+++ RPL/content.php
@@ -14,6 +14,15 @@
    else if($_GET["cm"]=="d-19"){
     include "d-19.php";
   }
+  else if($_GET["cm"]=="d-20"){
+    include "d-20.php";
+  }
+  else if($_GET["cm"]=="d-21"){
+    include "d-21.php";
+  }
+  else if($_GET["cm"]=="d-22"){
+    include "d-22.php";
+  }
    else if($_GET["cm"]=="d-25"){
     include "d-25.php";
   }
