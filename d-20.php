173766d78ff3cf4b206839aec0de8c67bbf08c23
Afrijal Marsa <afrijalmarsa2@gmail.com>
2013-06-13 05:06:14 -0600

    d-20.php

---
+++ d-20.php
@@ -0,0 +1,62 @@
+<fieldset style="border:none;">
+  <h2 align="left">Data Dosen Tahun Ajaran 2011/2012</h2>
+	<li align="left">Dr. Taufik Fuadi Abidin, M.Tech</li><br>
+	<table border='1' cellspacing="0" cellpadding="8px" align="left">
+		<tr style="font-weight:bold;">
+			<td>No</td>
+			<td align="center">Mata Kuliah</td>
+			<td align="center">Ruang</td>
+			<td align="center">Jam</td>
+			<td align="center">Keterangan</td>
+			<td align="center">Jumlah</td>
+		</tr>
+		<tr>
+			<td align='center'>1.</td>
+			<td>Pemrograman</td>
+			<td>R204</td>
+			<td>08.00 s/d 10.00 wib</td>
+			<td>INF IV</td>
+			<td>18 | 20</td>
+		</tr>
+		<tr>
+			<td align='center'>2.</td>
+			<td>Struktur Data</td>
+			<td>R304</td>
+			<td>10.00 s/d 12.00 wib</td>
+			<td>INF III</td>
+			<td>20 | 20</td>
+		</tr>
+	</table>
+</fieldset>
+<fieldset style="border:none;">
+	<br><br>
+	<li align="left">Irvanizam zamanhuri, M.Sc</li><br>
+	<table border='1' cellspacing="0" cellpadding="8px" align="left">
+		<tr style="font-weight:bold;">
+			<td>No</td>
+			<td align="center">Mata Kuliah</td>
+			<td align="center">Ruang</td>
+			<td align="center">Jam</td>
+			<td align="center">Keterangan</td>
+			<td align="center">Jumlah</td>
+		</tr>
+		<tr>
+			<td align='center'>1.</td>
+			<td>Kecerdasan Buatan</td>
+			<td>R304</td>
+			<td>08.00 s/d 10.00 wib</td>
+			<td>INF II</td>
+			<td>20 | 20</td>
+		</tr>
+		<tr>
+			<td align='center'>2.</td>
+			<td>Basis Data</td>
+			<td>R205</td>
+			<td>14.00 s/d 16.00 wib</td>
+			<td>INF V</td>
+			<td>17 | 20</td>
+		</tr>
+	</table>
+</fieldset>
+<br></br>
+	<p align="left"><input type="reset" value="Kembali" class="button" onclick="history.go(-1);"></p>
