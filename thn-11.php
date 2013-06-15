2c0d5472737879ee4ba7e45a93f5724f64d5224f
YuliaMiska <yulia_miska@yahoo.com>
2013-06-15 14:14:38 +0700

    thn-11.php

---
+++ thn-11.php
@@ -0,0 +1,46 @@
+<fieldset style="border:none;">
+  <h2 align="left">Data Mahasiswa Tahun Ajaran 2011/2012</h2>
+	<li align="left">Dwi Novitasari</li><br>
+	<table border='1' cellspacing="0" cellpadding="8px" align="left">
+		<tr style="font-weight:bold;">
+			<td>No</td>
+			<td align="center">Mata Kuliah</td>
+			<td align="center">Ruang</td>
+			<td align="center">Jam</td>
+			<td align="center">Keterangan</td>
+			<td align="center">Jumlah</td>
+		</tr>
+		<tr>
+			<td align='center'>1.</td>
+			<td>Pemrograman</td>
+			<td>R204</td>
+			<td>08.00 s/d 10.00 wib</td>
+			<td>INF IV</td>
+			<td>18 | 20</td>
+		</tr>
+	</table>
+</fieldset>
+<fieldset style="border:none;">
+	<br><br>
+	<li align="left">Yulia Miska</li><br>
+	<table border='1' cellspacing="0" cellpadding="8px" align="left">
+		<tr style="font-weight:bold;">
+			<td>No</td>
+			<td align="center">Mata Kuliah</td>
+			<td align="center">Ruang</td>
+			<td align="center">Jam</td>
+			<td align="center">Keterangan</td>
+			<td align="center">Jumlah</td>
+		</tr>
+		<tr>
+			<td align='center'>1.</td>
+			<td>Kecerdasan Buatan</td>
+			<td>R304</td>
+			<td>08.00 s/d 10.00 wib</td>
+			<td>INF II</td>
+			<td>20 | 20</td>
+		</tr>
+	</table>
+</fieldset>
+<br>
+	<p align="left"><input type="reset" value="Kembali" class="button" onclick="history.go(-1);"></p>
