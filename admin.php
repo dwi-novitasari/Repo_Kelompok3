eeeac0d48f2e4ee14c7563310999ebce80106a57
unknown <abbas.azzuhri@s1.informatika.unsyiah.ac.id>
2013-06-15 15:51:05 +0800

    Diupload oleh Abbas Adam Az-Zuhri

---
+++ admin.php
@@ -0,0 +1,87 @@
+<html>
+<head>
+<title>RPL</title>
+</head>
+<body>
+  <fieldset style="border:none;">
+  <h2 align="left">Admin</h2>
+  <p align="right" style="font-size:13px;"><a href="index.php">Logout</a></p>
+  	<table width="90%" align="left" class="tabel">
+	 <tr>
+	  <td>
+	    <table>
+		  <tr>
+		  <td><h4>Kirim Data</h4></td>
+		  </tr>
+		  <tr>
+		  <td>
+		   <table width="100%">
+		    <tr>
+			  <td align="left" width="20%">
+				<img src="send.png" width="80px" height="80px">
+			 </td>
+			 <td width="80%" align="left">
+				<p>Anda dapat melakukan pengiriman data Mahasiswa atau Dosen ke Fakultas atau PUKSI.</p>
+				<li><a href="?cm=krm">Kirim</a></li>
+			</td>
+			</tr>
+    	   </table>
+		  </td>
+		  </tr>
+		</table>
+	  </td>
+	 </tr>
+	 <tr></tr>
+	 <tr>
+	  <td>
+	    <table>
+		  <tr>
+		  <td><h4>Edit Data</h4></td>
+		  </tr>
+		  <tr>
+		  <td>
+		   <table width="100%">
+		    <tr>
+			  <td align="left" width="20%">
+				<img src="edit.png" width="80px" height="80px">
+			 </td>
+			<td width="80%" align="left">
+				<p>Anda dapat melakukan pengubahan terhadap data Absensi.</p>
+				<li><a href="?cm=ed">Ubah Data</a></li>
+			 </td>
+			</tr>
+    	   </table>
+		  </td>
+		  </tr>
+		</table>
+	  </td><br>
+	 </tr>
+	 <tr></tr>
+	 <tr>
+	  <td>
+	    <table>
+		  <tr>
+		  <td><h4>Lihat Data</h4></td>
+		  </tr>
+		  <tr>
+		  <td>
+		   <table width="100%">
+		    <tr>
+			  <td align="left" width="20%">
+				<img src="show.png" width="80px" height="80px">
+			 </td>
+			<td width="80%" align="left">
+				<p>Anda dapat melihat terhadap data Dosen dan Mahasiswa secara keseluruhan.</p>
+				<li><a href="?cm=lht">Lihat Data</a></li>
+			 </td>
+			</tr>
+    	   </table>
+		  </td>
+		  </tr>
+		</table>
+	  </td>
+	 </tr>
+	</table>
+   </fieldset>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<html>
+<head><font face="calibri">
+  <title>RPL</title>
+  <link href="desain.css" rel="stylesheet" type="text/css">	
+</head>
+<body style="background:url(bg.jpg);"><center>
+<!-- page -->
+<div id="page">
+<!-- header -->
+<div id="header">
+  <h1 align="left">
+	<table width="100%">
+	  <tr>
+	    <td align="left" width="100px"><img src="Unsyiah-logo.png" width="100px" height="100px"></td>
+		<td align="left">
+		<table align="left">
+		  <tr><td style="font-size:35px; color:white;"><b>Absensi Elektronik Finger Print</b></h1></td>
+		  <td style="font-size:20px; text-indent:20px; color:white;"><b><i>Jurusan Informatika</i></b></td></tr>
+		  <tr><td style="font-size:18px; color:white;" align="center">Fakultas Matematika & Ilmu Pengetahuan Alam</td></tr>
+		  <tr><td style="font-size:18px; color:white;" align="center">Universitas Syiah Kuala</td></tr>
+		</table></td>
+	  </tr>
+	</table>
+</div>
+<div id="menu">
+  <ul>
+	<li><a href="?cm=hm">Home</a></li>
+	<li><a href="#">Data Dosen</a>
+	  <ul>
+		<li><a href="?cm=d-19">Tahun Ajaran 2010/2011</a></li>
+		<li><a href="?cm=d-20">Tahun Ajaran 2011/2012</a></li>
+		<li><a href="?cm=d-21">Tahun Ajaran 2012/2013</a></li>
+	  </ul>
+	</li>
+	<li><a href="#">Data Mahasiswa</a>
+	  <ul>
+		<li><a href="?cm=ang-10">Angkatan 2010</a></li>
+		<li><a href="?cm=ang-11">Angkatan 2011</a></li>
+		<li><a href="?cm=ang-12">Angkatan 2012</a></li>
+	  </ul>
+	</li>
+	<li><a href="?cm=ln">Login</a></li>
+  </ul>
+</font>
+</div>
+<div id="content">
+ 
+</div>
+
+ <div id="isi">
+    <?php include "content.php"; ?>
+</div>
+
+
+<div id='footer'>
+  <p align='center'>Kelompok 3 RPL</p>
+</div>  
+</div>
+</center>
+</body>
+</font>
+</html>
