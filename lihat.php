8306883164f5ee7071e7aa491a2b99272bfb8a61
YuliaMiska <yulia_miska@yahoo.com>
2013-06-15 14:13:51 +0700

    lihat.php

---
+++ lihat.php
@@ -0,0 +1,77 @@
+<h2 align="left">Edit Data</h2>
+<p align="right" style="font-size:13px;"><a href="index.php">Logout</a></p>
+<fieldset style="border:none;">
+  <br><br>
+	<table border='1' cellspacing="0" cellpadding="8px" align="left">
+		<tr>
+			<td>No</td>
+			<td align="center">Nama</td>
+			<td align="center">NIP</td>
+			<td align="center">Mata Kuliah</td>
+			<td align="center">Ruang</td>
+			<td align="center">Hari/Tanggal</td>
+			<td align="center">Jam</td>
+			<td align="center">Keterangan</td>
+			<td align="center">Jumlah Absensi</td>
+		</tr>
+		<tr>
+			<td align='center'>1.</td>
+			<td>Dr Taufik Fuadi Abidin, M.Tech</td>
+			<td>1234567890</td>
+			<td>Pemograman</td>
+			<td>R304</td>
+			<td>Senin, 20 Juni 2013</td>
+			<td>08.00 s/d 10.00 wib</td>
+			<td>INF II</td>
+			<td>19 | 20</td>
+		</tr>
+		<tr>
+			<td align='center'>2.</td>
+			<td>Dr Taufik Fuadi Abidin, M.Tech</td>
+			<td>1234567890</td>
+			<td>Text Mining</td>
+			<td>R205</td>
+			<td>Rabu, 22 Juni 2013</td>
+			<td>08.00 s/d 10.00 wib</td>
+			<td>INF V</td>
+			<td>20 | 20</td>
+		</tr>
+	</table>
+</fieldset><fieldset style="border:none;">
+	<br><br>
+	<table border='1' cellspacing="0" cellpadding="8px" align="left">
+		<tr>
+			<td>No</td>
+			<td align="center">Nama</td>
+			<td align="center">NIP</td>
+			<td align="center">Mata Kuliah</td>
+			<td align="center">Ruang</td>
+			<td align="center">Hari/Tanggal</td>
+			<td align="center">Jam</td>
+			<td align="center">Keterangan</td>
+			<td align="center">Jumlah Absensi</td>
+		</tr>
+		<tr>
+			<td align='center'>1.</td>
+			<td>Irvanizam Zamanhuri, M.Sc</td>
+			<td>1234567890</td>
+			<td>Kecerdasan Buatan</td>
+			<td>R304</td>
+			<td>Selasa, 21 Juni 2013</td>
+			<td>08.00 s/d 10.00 wib</td>
+			<td>INF II</td>
+			<td>18 | 20</td>
+		</tr>
+		<tr>
+			<td align='center'>2.</td>
+			<td>Irvanizam Zamanhuri, M.Sc</td>
+			<td>1234567890</td>
+			<td>Basis Data</td>
+			<td>R205</td>
+			<td>Jum'at, 24 Juni 2013</td>
+			<td>14.00 s/d 16.00 wib</td>
+			<td>INF V</td>
+			<td>19 | 20</td>
+		</tr>
+	</table>
+</fieldset>
