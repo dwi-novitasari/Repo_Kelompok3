44344950931cfe2946e9a402403c168307b4a9c5
fadhil12 <fadhil.noer12@gmail.com>
2013-06-15 00:02:54 -0600

    ok.php

---
+++ ok.php
@@ -0,0 +1,8 @@
+   <h2 align="left">Terimakasih</h2>
+   <p align="right" style="font-size:13px;"><a href="index.php">Logout</a></p>
+  <table cellpadding="10" width="10%" style="background:Lightgreen;">
+		<tr><td><img src="check.png" width="20px" height="20px"></td>
+		<td style="font-weight:bold; color:darkgreen;">Succesfully</td></tr>
+	</table>
+	<br><br>
+    <p align="left"><input type="reset" value="Kembali" class="button" onclick="history.go(-1);"></p>
