11fca4ed6cc5163d0321dcb7d7ed72256bc9c4a8
Afrijal Marsa <afrijalmarsa2@gmail.com>
2013-06-15 13:19:01 +0700

    diposkan oleh

---
+++ ang-11.php
@@ -0,0 +1,5 @@
+<h2 align="left">Angkatan 2011</h2>
+<li align="left"><a href="?cm=thn-11">Tahun Ajaran 2011/2012</a></li>
+<li align="left"><a href="?cm=thn-12">Tahun Ajaran 2012/2013</a></li>
+<br>
+  <p align="left"><input type="reset" value="Kembali" class="button" onclick="history.go(-1);"></p>
