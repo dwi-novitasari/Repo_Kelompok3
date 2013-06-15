d87c97319a1cc9fe1b638a29cc1661a5dac55bf7
Ridha Akbar <ridha.akbar1993@gmail.com>
2013-06-15 13:08:39 +0700

    thn-10.php

---
+++ thn-10.php
@@ -0,0 +1,46 @@
+<fieldset style="border:none;">
+  <h2 align="left">Data Mahasiswa Tahun Ajaran 2010/2011</h2>
+	<li align="left">M Ikhwan Maulana</li><br>
+	<table border='1' cellspacing="0" cellpadding="8px" align="left">
+		<tr style="font-weight:bold;">
+			<td>No</td>
+			<td align="center">Mata Kuliah</td>
+			<td align="center">Ruang</td>
+			<td align="center">Jam</td>
+			<td align="center">Keterangan</td>
+			<td align="center">Jumlah</td>
+		</tr>
+		<tr>
+			<td align='center'>2.</td>
+			<td>Struktur Data</td>
+			<td>R304</td>
+			<td>10.00 s/d 12.00 wib</td>
+			<td>INF III</td>
+			<td>20 | 20</td>
+		</tr>
+	</table>
+</fieldset>
+<fieldset style="border:none;">
+	<br><br>
+	<li align="left">Nawar Nabila Zi</li><br>
+	<table border='1' cellspacing="0" cellpadding="8px" align="left">
+		<tr style="font-weight:bold;">
+			<td>No</td>
+			<td align="center">Mata Kuliah</td>
+			<td align="center">Ruang</td>
+			<td align="center">Jam</td>
+			<td align="center">Keterangan</td>
+			<td align="center">Jumlah</td>
+		</tr>
+		<tr>
+			<td align='center'>2.</td>
+			<td>Basis Data</td>
+			<td>R205</td>
+			<td>14.00 s/d 16.00 wib</td>
+			<td>INF V</td>
+			<td>17 | 20</td>
+		</tr>
+	</table>
+</fieldset>
+<br>
+	<p align="left"><input type="reset" value="Kembali" class="button" onclick="history.go(-1);"></p>
